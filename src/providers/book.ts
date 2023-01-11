74e7264a3d592826c3c38b480757fd59ddd2f10e
Armand Marechal <sypenfu@gmail.com>
2023-01-11 15:45:13 +0100

    Instantiate model from fetching

--- src/providers/book.ts
+++ src/providers/book.ts
@@ -1,4 +1,4 @@
-import type Book from '@/models/book';
+import Book from '@/models/book';
 import axios from 'axios';
 
 export default class BookProvider {
@@ -14,8 +14,8 @@ static async searchBooks(query: string, max: number = 10, offset: number = 0): P
 
   static async fetchBooks(max: number = 10, offset: number = 0): Promise<Book[]> {
     try {
-      const response = await axios.get('/books?max=' + max + '&offset=' + offset);
-      return response.data;
+      const books = (await axios.get('/books?max=' + max + '&offset=' + offset)).data;
+      return books.data.items.map((book: JSON) => Book.fromJson(book));
     } catch (error) {
       console.error(error);
       throw error;
