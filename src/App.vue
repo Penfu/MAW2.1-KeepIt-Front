4b3bfb013ce6cdfde552de82417cb9d16fe9abf8
Armand Marechal <sypenfu@gmail.com>
2023-02-23 10:24:54 +0100

    Add logout btn

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,15 @@ const auth = useAuthStore();
             <RouterLink to="/books">Books</RouterLink>
           </div>
 
-          <span v-if="auth.isAuth">Logged as user X</span>
+          <div v-if="auth.isAuth" class="space-x-4">
+            <span>Logged as user X</span>
+            <button
+              @click="auth.logout()"
+              class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Logout
+            </button>
+          </div>
           <RouterLink v-else to="/login">Login</RouterLink>
         </nav>
       </div>
