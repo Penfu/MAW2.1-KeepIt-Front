b30c649f8bb1c48bf90b97dc78b0d241fc7c7db2
Armand Marechal <sypenfu@gmail.com>
2023-02-23 10:20:46 +0100

    Refactor login view

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import router from '@/router';
 import { reactive } from 'vue';
+import router from '@/router';
 import { RouterLink } from 'vue-router';
 import { email, required } from '@vuelidate/validators';
 import useVuelidate from '@vuelidate/core';
@@ -30,42 +30,61 @@ const onSubmit = async () => {
   }
 
   await auth.login(formData.email, formData.password);
-  router.push('/');
+
+  if (auth.isAuth) router.push('/');
 };
 </script>
 
 <template>
   <main class="mx-4 md:mx-8 lg:mx-16 xl:mx-32">
-    <div class="py-16 w-full flex items-center justify-center flex-col space-y-4">
+    <div
+      class="py-16 w-full flex items-center justify-center flex-col space-y-4"
+    >
       <div class="max-w-lg w-full space-y-12">
         <h2 class="text-3xl font-semibold uppercase text-center">Login</h2>
 
         <!-- Manual -->
         <form @submit.prevent="onSubmit" class="w-full flex flex-col space-y-6">
           <ErrorAlert :errors="auth.loginErrors" />
-          <div>
+
+          <div class="space-y-2">
             <ErrorAlert :errors="v$.email.$errors" />
-            <div>
-              <label class="block">Email</label>
-              <input type="email" v-model="formData.email"
-                class="w-full px-4 py-3 border-2 border-gray-200 rounded-lg text-lg" />
-            </div>
-          </div>
-          <div>
-            <ErrorAlert :errors="v$.password.$errors" />
-            <div>
-              <label class="block">Password</label>
-              <input type="password" v-model="formData.password"
-                class="w-full px-4 py-3 border-2 border-gray-200 rounded-lg text-lg" />
+
+            <div class="space-y-2">
+              <label>Email</label>
+              <input
+                type="email"
+                v-model="formData.email"
+                class="w-full px-4 py-3 border-2 border-gray-200 rounded-lg text-lg"
+              />
             </div>
           </div>
 
-          <button type="submit" class="w-full px-4 py-3 bg-blue-500 hover:bg-blue-600 text-white rounded text-lg">
+          <div class="space-y-2">
+            <ErrorAlert :errors="v$.password.$errors" />
+
+            <div class="space-y-2">
+              <label>Password</label>
+              <input
+                type="password"
+                v-model="formData.password"
+                class="w-full px-4 py-3 border-2 border-gray-200 rounded-lg text-lg"
+              />
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            class="w-full px-4 py-3 bg-blue-500 hover:bg-blue-600 text-white rounded text-lg"
+          >
             Login
           </button>
         </form>
 
-        <router-link to="/register" class="block text-gray-600 hover:text-gray-800 text-center">
+        <router-link
+          to="/register"
+          class="block text-gray-600 hover:text-gray-800 text-center"
+        >
           Not already register ?
         </router-link>
       </div>
