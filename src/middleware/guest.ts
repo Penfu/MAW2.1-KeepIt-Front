4ffe8a5cf65529f22be3b3ff895dc69798aa6e40
Anthony <bouillant.anthony@gmail.com>
2023-03-11 01:57:10 +0100

    Fix missing params guestMiddleware

--- src/middleware/guest.ts
+++ src/middleware/guest.ts
@@ -1,12 +1,14 @@
 import type { GuardType } from '@/router';
 import { useAuthStore } from '@/stores/auth';
+import type { RouteParamsRaw } from 'vue-router';
 
 export default function guest({ to, from, next }: GuardType): void {
   const authStore = useAuthStore();
   const isLoggedIn = authStore.isAuth;
   if (isLoggedIn) {
     return next({
-      name: (to.redirectedFrom ? to.redirectedFrom.name : 'login')?.toString(),
+      name: (to.redirectedFrom?.name ?? (to.name as string)) as string,
+      params: (to.redirectedFrom?.params ?? to.params) as RouteParamsRaw,
     });
   }
 
